0dfc1d52ac9bf0eb220a7e96586b84114ad088ce
ahmadowida2512 <aowida2512@gmail.com>
2025-03-09 19:46:48 +0200

    T5.cs

--- T5.cs
+++ T5.cs
@@ -1,24 +1,27 @@
 public class Solution {
-    public void Rotate(int[] nums, int k) {
+    public void Rotate(int[] nums, int ke) {
         int n = nums.Length;
-        k = k % n; 
+        ke = ke % n; 
 
 
         Reverse(nums, 0, n - 1);
 
     
-        Reverse(nums, 0, k - 1);
+        Reverse(nums, 0, ke - 1);
 
-        Reverse(nums, k, n - 1);
+        Reverse(nums, ke, n - 1);
     }
 
     private void Reverse(int[] nums, int start, int end) {
-        while (start < end) {
+        while (start < end)
+        {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
             start++;
             end--;
+            //done
+
         }
     }
 }
